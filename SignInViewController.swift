0f5c37821c63c81deba5273549fe348e43c68077
diane <dianecronenwett@gmail.com>
2014-10-19 21:32:14 -0700

    carousel

---
+++ SignInViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignInViewController.swift
+//  carousel
+//
+//  Created by diane cronenwett on 10/19/14.
+//  Copyright (c) 2014 dianesorg. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    @IBOutlet var backButton: UITapGestureRecognizer!
+   
+    @IBAction func backButton(sender: AnyObject) {
+         navigationController!.popViewControllerAnimated(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
